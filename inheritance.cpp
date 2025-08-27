22d05f05e0d91ed640c6e925c20fb32c50c6938a
paradox-shivansh <theaverageguy@gmail.com>
2025-08-27 22:01:32 +0530

    more about inheritance

--- inheritance.cpp
+++ inheritance.cpp
@@ -3,12 +3,51 @@
 // #include "inheritance.h"
 using namespace std;
 
+class Human{
+    public:
+    int height;
+    int weight;
+    int age;
 
+    // Height
+    void setheight(int height){
+       this->height = height;
+    }
+    int getheight(){
+        return height;
+    }
 
+    // Age
+    void setage(int age){
+        this->age = age;
+    }
+    int getage(){
+        return age;
+    }
+};
 
+//male class inherited from human class
+// ex. i inherited my genes from my parents
+class Male: public Human{
+    public:
+    string color;
+    
 
-int mmain () {
+    void sleep(){
+        cout << "the person is sleeping" << endl;
+    }
+
+    };
+
+int main () {
     cout << "Hello World!" << endl;
+
+    Male m1;
+    m1.setage(20);
+
+    cout << m1.getage() << endl;
+
+    m1.sleep();
 
     return 0;
     }
